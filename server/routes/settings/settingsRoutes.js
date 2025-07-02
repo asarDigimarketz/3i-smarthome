60778b089aefe8316553b50cfdc5da1a2768c003
dinesh-mnt <dinesh@mntfuture.com>
2025-07-02 16:55:25 +0530

    Delete server directory

--- server/routes/settings/settingsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const GeneralRoutes = require("./general/generalRoutes");
-const EmailConfigurationRoutes = require("./emailConfiguration/emailConfiguration.route");
-router.use("/general", GeneralRoutes);
-router.use("/emailConfiguration", EmailConfigurationRoutes);
-
-module.exports = router;
